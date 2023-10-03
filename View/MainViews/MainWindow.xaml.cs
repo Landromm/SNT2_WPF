cfdd1b36eab09f783b2f0ad038e67f9accad4acf
Igor Radkevich <landrommigor@gmail.com>
2023-10-03 16:41:15 +0300

    -add MainWindow, with animation, without hidden button

--- View/MainViews/MainWindow.xaml.cs
+++ View/MainViews/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +22,41 @@ namespace SNT2_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        WindowState prevState;
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();  
         }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void grdHeader_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+            else
+                prevState = WindowState;
+        }
+
+        private void TaskbarIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = prevState;
+        }
+
+
     }
 }
---
+++ ViewModel/MainViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SNT2_WPF.ViewModel.MainViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private bool _checkActivetedHideMenu=false;
+
+        //Properties
+        public bool CheckActivetedHideMenu
+        {
+            get => _checkActivetedHideMenu;
+            set
+            {
+                _checkActivetedHideMenu = value;
+                OnPropertyChanged(nameof(CheckActivetedHideMenu));
+            }
+        }
+
+
+        public ICommand ActivateHideMenuCommand { get; }
+
+        public MainViewModel()
+        {
+            ActivateHideMenuCommand = new ViewModelCommand(ExecuteActivateHideMenuCommand);
+        }
+
+        private void ExecuteActivateHideMenuCommand(object obj)
+        {
+            CheckActivetedHideMenu = !CheckActivetedHideMenu;
+        }
+    }
+}
