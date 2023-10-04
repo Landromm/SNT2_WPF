f5fa6cf00afc4d1fff09f0cab3d313169a254789
Igor Radkevich <landrommigor@gmail.com>
2023-10-04 12:05:08 +0300

    -start create UserControl for ItemListView

---
+++ Styles/CustomControls/ItemListCounter.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SNT2_WPF.Styles.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for ItemListCounter.xaml
+    /// </summary>
+    public partial class ItemListCounter : UserControl
+    {
+        public ItemListCounter()
+        {
+            InitializeComponent();
+        }
+    }
+}
