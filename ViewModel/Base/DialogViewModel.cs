60c6e7f64dcefa9eb1f2899a13ebc03806833fa0
Igor Radkevich <landrommigor@gmail.com>
2023-10-20 14:04:54 +0300

    Добавлен базовый интерфейс окон с отслеживанием событий.

---
+++ ViewModel/Base/DialogViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNT2_WPF.ViewModel.Base;
+public class DialogViewModel : ViewModel
+{
+	public event EventHandler? DialogComplete;
+	protected virtual void OnDialogComplete(EventArgs e) => DialogComplete?.Invoke(this, e);
+}
