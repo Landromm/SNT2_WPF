4e667ddaf564c493c429c9c0e28d24c1329db38e
Igor Radkevich <landrommigor@gmail.com>
2023-12-08 15:52:55 +0300

    Старт кодирования ViewModel для ItemTabControl.

--- ViewModel/Settings/UserControlViewModel/ItemTabControlTagViewModel.cs
+++ ViewModel/Settings/UserControlViewModel/ItemTabControlTagViewModel.cs
@@ -7,35 +7,37 @@ using System.Threading.Tasks;
 namespace SNT2_WPF.ViewModel.Settings.UserControlViewModel;
 internal class ItemTabControlTagViewModel : Base.ViewModel
 {
-	
+	//Инициализация наименования вкладок в "TabControl".
+    public string TabName { get; init; }
+
+    #region CounterNumber : string? - Номер Счетчика в списке.
+
+    /// <summary>Номер Счетчика в списке. - поле.</summary>
+    private string? _counterNumber;
+
+	/// <summary>Номер Счетчика в списке. - свойство.</summary>
+	public string? CounterNumber
+	{
+		get => _counterNumber;
+		set
+		{
+			_counterNumber = value;
+			OnPropertyChanged(nameof(CounterNumber));
+		}
+	}
+	#endregion
+
+
+
 
 	public ItemTabControlTagViewModel(string tabName, string countName)
 	{
 		TabName = tabName;
-		Name = countName;
+		CounterNumber = countName;
 	}
 
-	public string TabName
-	{
-		get;
-		private set;
-	}
 
-	private string name;
-	public string Name
-	{
-		get
-		{
-			return name;
-		}
 
-		set
-		{
-			if (name != value)
-			{
-				name = value;
-				OnPropertyChanged("Name");
-			}
-		}
-	}
+
+
 }
