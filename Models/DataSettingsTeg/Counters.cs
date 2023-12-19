a8490ab41fee9460184160e119da6fe0977042c3
Igor Radkevich <landrommigor@gmail.com>
2023-12-19 16:01:07 +0300

    Добавлена десериализация и сериализация JSON настроек каналов счтчиков.

---
+++ Models/DataSettingsTeg/Counters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNT2_WPF.Models.DataSettingsTeg;
+internal class Counters
+{
+    public List<SettingCounters>? CountersList { get; set; }
+}
